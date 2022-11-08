b65a9911781ca9cc55c7de0bb22da1c093b8a7f8
Killipok <92814934+Killipok@users.noreply.github.com>
2022-11-09 02:59:44 +0700

    Add files via upload

--- matrix/main.c
+++ matrix/main.c
@@ -16,8 +16,31 @@ int main()
     matrix_t neo;
     neo = create_matrix(rows, cols);
     fill_matrix(&neo);
+
     print_matrix(&Morfius);
+    printf("\n");
     print_matrix(&neo);
+
+    printf("Сумма матриц - \n");
+    matrix_t sum = sum_matrix(&neo, &Morfius);
+    print_matrix(&sum);
+
+    printf("Разница матриц - \n");
+    matrix_t sub = sub_matrix(&neo, &Morfius);
+    print_matrix(&sub);
+
+    printf("Умножение матрицы на число - \n");
+    matrix_t mult = mult_num(&neo, &Morfius);
+    print_matrix(&mult);
+
+    printf("Умножение матриц - \n");
+    matrix_t mult_m = mult_matrix(&neo, &Morfius);
+    print_matrix(&mult_m);
+
+    printf("Меняет местами   - \n");
+    matrix_t to_swap = swap(&neo, &Morfius);
+    print_matrix(&to_swap);
+
     /* int res = eq_size(&neo, &Morfius);
      printf("res = %d \n", res);
      int equals = eq_matrix(&neo, &Morfius);
