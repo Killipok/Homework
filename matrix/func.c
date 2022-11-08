15815cc9991aa5c011acd117d8cc22ca808267cd
Killipok <rad_83@mail.ru>
2022-11-08 19:10:24 +0700

    added dir

--- matrix/func.c
+++ matrix/func.c
@@ -1,4 +1,5 @@
 #include "func.h"
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -43,33 +44,104 @@ void fill_matrix(matrix_t *matrix)
     }
 }
 
-void mult_matrix(matrix_t *neo, matrix_t *Morfius) {}
-
-/*int eq_size(matrix_t *neo, matrix_t *Morfius)
+int eq_size(matrix_t *neo, matrix_t *Morfius)
 {
     int res = 0;
     if (neo->rows == Morfius->rows && neo->cols == Morfius->cols) {
-        res = 0;
-        return res;
+        res = 1;
     }
     return res;
 }
 
 int eq_matrix(matrix_t *neo, matrix_t *Morfius)
 {
-    int res = 0;
+    int res = 1;
     if (eq_size(neo, Morfius)) {
         for (int i = 0; i < neo->rows; ++i) {
             for (int j = 0; j < neo->cols; ++j) {
-                if (neo->matrix[i][j] == Morfius->matrix[i][j]) {
-                } else {
-                    return res;
+                if (neo->matrix[i][j] != Morfius->matrix[i][j]) {
+                    res = 0;
+                    break;
                 }
+            }
+            if (res == 0) {
+                break;
             }
         }
     } else {
-        ++res;
-        return res;
+        res = 0;
     }
-    return 0;
-}*/
+    return res;
+}
+
+matrix_t sum_matrix(matrix_t *A, matrix_t *B)
+{
+    matrix_t neo;
+    if (eq_size(A, B)) {
+        neo = create_matrix(A->rows, A->cols);
+        for (int i = 0; i < A->rows; ++i) {
+            for (int j = 0; j < B->cols; ++j) {
+                neo.matrix[i][j] = A->matrix[i][j] + B->matrix[i][j];
+            }
+        }
+    }
+    return neo;
+}
+
+matrix_t sub_matrix(matrix_t *A, matrix_t *B)
+{
+    matrix_t neo;
+    if (eq_size(A, B)) {
+        neo = create_matrix(A->rows, B->cols);
+        for (int i = 0; i < A->rows; ++i) {
+            for (int j = 0; j < B->cols; ++j) {
+                neo.matrix[i][j] = A->matrix[i][j] - B->matrix[i][j];
+            }
+        }
+    }
+    return neo;
+}
+
+matrix_t mult_num(matrix_t *A, matrix_t *B)
+{
+    matrix_t neo;
+    if (eq_size(A, B)) {
+        neo = create_matrix(A->rows, B->cols);
+        for (int i = 0; i < A->rows; ++i) {
+            for (int j = 0; j < B->cols; ++j) {
+                neo.matrix[i][j] = A->matrix[i][j] * 5;
+            }
+        }
+    }
+    return neo;
+}
+
+matrix_t mult_matrix(matrix_t *A, matrix_t *B)
+{
+    matrix_t neo;
+    if (eq_size(A, B)) {
+        neo = create_matrix(A->rows, B->cols);
+        for (int i = 0; i < A->rows; ++i) {
+            for (int j = 0; j < B->cols; ++j) {
+                for (int k = 0; k < B->cols; ++k) {
+                    neo.matrix[i][j] += A->matrix[i][k] * B->matrix[k][j];
+                }
+            }
+        }
+    }
+    return neo;
+}
+
+matrix_t swap(matrix_t *A, matrix_t *B)
+{
+    matrix_t neo;
+    if (eq_size(A, B)) {
+        neo = create_matrix(A->rows, B->cols);
+        for (int i = 0; i < A->rows; ++i) {
+            for (int j = 0; j < B->cols; ++j) {
+                neo.matrix[i][j] = B->matrix[j][i];
+            }
+        }
+    }
+    return neo;
+}
